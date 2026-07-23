08fdb183b363874e7ae4375aa6e6d8eba676325d
Harsha Vardhini <gajulaharshavardhini356@gmail.com>
2026-07-23 19:40:09 +0530

    done

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+class main{
+    public static void main(String[] args){
+        HashMap<String, Integer> maplist = new HashMap<>();
+        maplist.put("a",10);
+        maplist.put("b", 12);
+        maplist.put("c", 13);
+    
+    System.out.println(maplist.get("a"));
+
+    }
+}
